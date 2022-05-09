aab5fd58488f05c70a0b47cd60eb01b6b58ada09
Dragondev-King <dragondev1017@gmail.com>
2022-05-10 02:44:45 +0900

    create moralis calling functions

---
+++ app/controllers/index.js
@@ -0,0 +1,71 @@
+const axios = require("axios")
+
+const db = require("../models")
+const NftItem = db.table.NftItem
+const Owner = db.table.Owner
+
+async function getAllItems() {
+  console.log('---------')
+  let cursor = ""
+  do {
+    try {
+      const { data } = await axios(`https://deep-index.moralis.io/api/v2/nft/${process.env.PIZZA_NFT_CONTRACT_ADDRESS}?chain=bsc&format=decimal&cursor=${cursor}`, {
+      headers: {
+        'x-api-key': process.env.MORALIS_KEY
+      }
+    })
+
+    for (const item of data.result) {
+      await NftItem.findOneAndUpdate({tokenId: item.token_id}, {
+        tokenId: item.token_id,
+      },{
+        new: true,
+        upsert: true
+      })
+    }
+    cursor = data.cursor
+    } catch (err) {
+      console.log(err)
+    }
+  } while (cursor != '' && cursor != null)
+}
+
+
+async function getAllOwners() {
+  console.log('==============')
+  let cursor = ""
+  do {
+    try {
+      const { data } = await axios(`https://deep-index.moralis.io/api/v2/nft/${process.env.PIZZA_NFT_CONTRACT_ADDRESS}/owners?chain=bsc&format=decimal&cursor=${cursor}`, {
+      headers: {
+        'x-api-key': process.env.MORALIS_KEY
+      }
+    })
+
+    for (const owner of data.result) {
+      await Owner.findOneAndUpdate({tokenId: owner.token_id, tokenAddress: owner.token_address, ownerOf: owner.owner_of}, {
+        tokenAddress:owner.token_address,
+        tokenId: owner.token_id,
+        ownerOf: owner.owner_of
+      },{
+        new: true,
+        upsert: true
+      })
+    }
+    cursor = data.cursor
+    } catch (err) {
+      console.log(err)
+    }
+  } while (cursor != '' && cursor != null)
+}
+
+const callRequestToMoralis = () => {
+  setInterval(() => {
+    getAllItems()
+    getAllOwners()
+  }, 10000)
+}
+
+module.exports = {
+  callRequestToMoralis,
+}
